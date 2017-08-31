2153a0a013c3d5f3c0c3f06de81c99b82847423f
Unknown <emyamodio@gmail.com>
2017-08-30 20:31:00 -0600

    Test_tube Classes CLASSES

---
+++ DGM1600_Game/Assets/Classes.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Classes : MonoBehaviour {
+
+	public class One {
+		public int money = 1000000;
+		public float fuel = 0.01f;
+
+		private string name = "Bob";
+	
+
+	}
+
+	public class Two {
+		public string color = "blue";
+
+		public float water = 84.35f;
+
+		private int space = 54308;
+
+		
+	}
+
+	public class Three {
+
+		public string space = "forest";
+
+		private string title = "ruf";
+
+		public decimal number = 465;
+		
+
+	}
+}
+
