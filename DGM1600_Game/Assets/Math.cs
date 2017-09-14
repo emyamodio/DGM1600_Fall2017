df41e1736b6dc3115d3519ad8e1c89054161f5fa
Unknown <emyamodio@gmail.com>
2017-09-13 20:01:45 -0600

    9/13/17

---
+++ DGM1600_Game/Assets/Math.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Math : MonoBehaviour {
+
+	public int valueOne;
+
+	public int valueTwo;
+
+	private int result;
+
+		
+	// Use this for initialization
+	void Start () {
+		DoMath();
+		DoMath2();
+		DoMath3();
+		DoMath4();
+	}
+	
+	void DoMath (){
+		result = valueOne + valueTwo;
+		print(valueOne + " + " + valueTwo + " = " + result);
+	}
+
+	void DoMath2 (){
+		result = valueOne - valueTwo;
+		print(valueOne + " - " + valueTwo + " = " + result);
+	}
+
+	void DoMath3 (){
+		result = valueOne * valueTwo;
+		print(valueOne + " * " + valueTwo + " = " + result);
+	}
+
+	void DoMath4 (){
+		result = valueOne / valueTwo;
+		print(valueOne + " / " + valueTwo + " = " + result);
+	}
+
+	// Update is called once per frame
+	//void Update () {
+		
+	//}
+}
