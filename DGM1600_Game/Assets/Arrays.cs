08cc83bdbe06b778f1f95c8d8833c6a7ded479c0
Unknown <emyamodio@gmail.com>
2017-09-20 20:11:04 -0600

    Intro to Unity

---
+++ DGM1600_Game/Assets/Arrays.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrays : MonoBehaviour {
+
+	public string[] heros = new string[6];
+
+	
+	// Use this for initialization
+	void Start () {
+	heros[0] = "Batman";
+	heros[1] = "Superman";
+	heros[2] = "Wonder Woman";
+	heros[3] = "Green Lantern";
+	heros[4] = "Hawkman";
+	heros[5] = "Bat Girl";
+
+	print \(heros{0});	
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
