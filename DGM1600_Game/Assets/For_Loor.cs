1157fe396083bb9154f99d166681f8b31286212c
Unknown <emyamodio@gmail.com>
2017-10-02 19:50:25 -0600

    FOR LOOPS

---
+++ DGM1600_Game/Assets/For_Loor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class For_Loor : MonoBehaviour {
+	public int bottles = 100;
+	// Use this for initialization
+	void Start () {
+	// 	while(bottles > 0){
+	// 		print(bottles + " of orange crush on the wall");
+	// 		// bottles = bottles --;
+	// 	}
+
+	// for(int bottles = 100; bottles > 0; bottles --){
+	// 	print(bottles + " of orange chrush on the wall");
+	
+	for(; bottles > 0; bottles --){
+		print(bottles + " of orange chrush on the wall");
+	}
+	}
+	
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
