32d88568e06928b17ea5ac1a7e9380e93eeee25d
Unknown <emyamodio@gmail.com>
2017-10-08 22:03:59 -0600

    WhileLoop

--- DGM1600_Game/Assets/While.cs
+++ DGM1600_Game/Assets/While.cs
@@ -4,19 +4,74 @@ using UnityEngine;
 
 public class While : MonoBehaviour {
 
-	public int bottles = 100;
+//It's an assembly line that keeps going through the same task until it satisfies the task or a creates an infinite loop
+	
+	// public int bottles = 100;
+	// public int chocolates = 10;
+	// public int falcons = 67;
+	// public int texts = 12;
+	// public int people = 22;
+	// public int cellphones = 25;
+	// public int hats = 18;
+	// public int cake = 8;
+	// public int shoes = 2;
+	public int trees = 50;
 
 	// Use this for initialization
 	void Start () {
-		while(bottles > 0){
-			bottles --;
-			print(bottles + " bottles of beer on the wall");
-			
+		// while(bottles > 0){
+		// 	bottles --;
+		// 	print(bottles + " bottles of beer on the wall");
+		// }
+
+		// while(chocolates > 0){
+		// 	chocolates --;
+		// 	print(" chocolate I steressed ate");
+		// }
+
+		// while(falcons < 0){
+		// 	falcons ++;
+		// 	print(" falcons swoop down");
+		// }
+
+		// 	while(texts > 0);
+		// 	 texts --;
+		// 	 print(" texts I sent out today");
+		// }
+
+		// while(people > 0){
+		// 	people --;
+		// 	print(" people crossing the street");
+		// }
+
+		// while(cellphones > 0){
+		// 	cellphones --;
+		// 	print(" sold in a day");
+		// }
+
+		// while(hats > 0){
+		// 	hats --;
+		// 	print(" hats that flew in the wind");
+		// }
+
+		// while(cake > 0){
+		// 	cake --;
+		// 	print(" slices of cake on the plater");
+		// }
+
+		// while(shoes > 0){
+		// 	shoes --;
+		// 	print(" pairs of shoes on the window pain");
+		// }
+
+		while(trees > 0){
+			trees --;
+			print(" trees she hit on her way down");
 		}
-	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update() {
 		
 	}
+}
 }
